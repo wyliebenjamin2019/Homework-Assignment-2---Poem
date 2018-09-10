425b591046edba1f3cac8a76bd96ce6fe5a0e076
wyliebenjamin2019 <42677949+wyliebenjamin2019@users.noreply.github.com>
2018-09-10 08:35:30 -0700

    Create main.java

---
+++ main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("l(a\n\nle\naf\nfa\n\nll\n\ns)\none\nl\n\niness");
+    }
+}
